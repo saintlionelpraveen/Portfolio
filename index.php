f1f119f7c1d66432a5952afee79f5ec116026923
Admin <admin@portfolio.local>
2026-03-05 15:35:23 +0530

    test: trigger Jenkins multibranch pipeline

--- index.php
+++ index.php
@@ -175,6 +175,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['send_message'])) {
         <ul class="nav-links">
 
             <li><a href="#home">Home</a></li>
+            <li><a href="#home">Test</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#fellowship">Fellowship</a></li>
             <li><a href="#projects">Works</a></li>
